fb19b69f5bd11083dc1010f91c701dcfe265db76
DESKTOP\hyeonmin <hyeonmin97@naver.com>
2021-11-19 15:32:49 +0900

    Update comment

--- window.py
+++ window.py
@@ -4,12 +4,12 @@ import serial
 import time
 #시리얼 연결
 ser = serial.Serial(
-    port='COM16',\
+    port='COM3',\
     baudrate=115200,\
     timeout=None)
 print("connected to: " + ser.portstr)
 
-#모니터 목록 출력
+#모니터 목록 출력 함수
 def printAllScreen():
     i = 0
     while True:
@@ -21,7 +21,7 @@ def printAllScreen():
             break
     return i
 
-#0번 선택(보조 모니터)
+#4번 선택(보조 모니터)
 monitor = win32.EnumDisplayDevices(None, 4) 
 
 
@@ -33,46 +33,46 @@ monitor = win32.EnumDisplayDevices(None, 4)
 rotation = 0
 rotation_flag = 0
 #현재 설정 저장, 실패시 0리턴
-dm = win32.EnumDisplaySettings(monitor.DeviceName, win32con.ENUM_CURRENT_SETTINGS)
+dm = win32.EnumDisplaySettings(monitor.DeviceName, win32con.ENUM_CURRENT_SETTINGS) #디스플레이 현새 세팅 불러오기
 ser.flush()
-for i in range(50):
+for i in range(50): #프로그램 실행시 각도가 낮게 잡히는 문제 수정을 위해 추가
         b = ser.readline()
 while True:
     try:
-        b = ser.readline()
-        past = float(b.decode())
+        b = ser.readline() #자이로센서의 현재 기준값 지정
+        past = float(b.decode()) #문자열을 float형으로 변경
         break
     except:
-        continue
+        continue #값이 아닐경우 값이 나올때까지 반복
     
 while True:
     b = ser.read(ser.inWaiting())
-    if(b == ''.encode()):
+    if(b == ''.encode()): #''를 읽었을시 다시 읽음
         continue
     try:
         num = float(b.decode())
         #print(num)
-        if abs(past - num)>10:
+        if abs(past - num)>10: #이전값과 10도 이상 차이날경우 값이 튕긴거로 판단, 값을 다시 읽음
             continue
         else:
-            past = num
-        if dm.DisplayOrientation == win32con.DMDO_DEFAULT: #기본상태일때
+            past = num #정상값일경우 이전값 저장
+        if dm.DisplayOrientation == win32con.DMDO_DEFAULT: #현재모니터 화면이 기본상태일때 (0도)
             if num > 50: #시계방향 50도 이상 회전시
                 rotation = win32con.DMDO_90
-                rotation_flag = 1
+                rotation_flag = 1 #회전 발생 플래그
             elif num < -50: #반시계방향 50도 이상 회전시
                 rotation = win32con.DMDO_270
-                rotation_flag = 1
+                rotation_flag = 1 #회전 발생 플래그
 
-        elif dm.DisplayOrientation == win32con.DMDO_90 : #90도 회전상태일때
-            if num < 40 : #원래 상태로 간주
+        elif dm.DisplayOrientation == win32con.DMDO_90 : #현재모니터 화면이 90도 회전상태일때(시계방향)
+            if num < 40 : #원래 상태로 돌렸다고 판단
                 rotation = win32con.DMDO_DEFAULT
-                rotation_flag = 1
+                rotation_flag = 1 #회전 발생 플래그
 
-        elif dm.DisplayOrientation == win32con.DMDO_270: #279도회전상태일때
-            if num > -40 : #원래상태로 간주
+        elif dm.DisplayOrientation == win32con.DMDO_270: #현재모니터 화면이 270도회전상태일때(반시계방향)
+            if num > -40 : #원래 상태로 돌렸다고 판단
                 rotation = win32con.DMDO_DEFAULT
-                rotation_flag = 1
+                rotation_flag = 1 #회전 발생 플래그
 
     except Exception as e:
         continue
@@ -81,13 +81,12 @@ while True:
 
 
     if rotation_flag == 1 :
-        #가로세로 변경
         if((dm.DisplayOrientation + rotation)%2 == 1):
+            #가로세로 변경
             dm.PelsWidth, dm.PelsHeight = dm.PelsHeight, dm.PelsWidth
 
-        dm.DisplayOrientation = rotation
-        win32.ChangeDisplaySettingsEx(monitor.DeviceName, dm)
+        dm.DisplayOrientation = rotation #현재 상태 저장
+        win32.ChangeDisplaySettingsEx(monitor.DeviceName, dm) #화면 변경
         rotation_flag = 0
 
     time.sleep(0.01)
-    #ser.flush()
